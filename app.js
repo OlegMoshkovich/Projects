55939ecf55153412addace0f92ff30fd22f4c435
OlegMoshkovich <oleg.mosh@gmail.com>
2017-09-14 11:03:59 -0400

    work route

--- app.js
+++ app.js
@@ -16,8 +16,6 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -28,6 +26,7 @@ app.use('/', index);
 app.use('/users', users);
 app.use('/thoughts', thoughts);
 app.use('/resume', cv);
+app.use('/work', index);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
